c7613e27f4bcc7c9d1c8decea7e0d8d04bd5c799
Marvin Vogel <91740473+marvinvo@users.noreply.github.com>
2022-08-21 19:18:31 +0200

    adjust write stats to file option

--- main.py
+++ main.py
@@ -87,9 +87,9 @@ def print_stats(s, func, end_event):
         stats_file = os.path.join(s[settings.ARG_RESULTFOLDER], "general_stats.log")
         while not end_event.is_set():
             stats = "{}   ......   {}".format(", ".join(["{}: {}".format(f["name"] + " success ratio", "{}/{}".format(f["count_value"].value, f["failed_value"].value + f["count_value"].value)) for f in func]), ", ".join(["{}: {}".format(f["name"] + " worker", f["worker_count"].value) for f in func]) )
-            with open(stats_file, "a+") as f:
+            with open(stats_file + "\n", "a+") as f:
                 f.write(stats)
-            sleep(5)
+            sleep(10)
     else:
         while not end_event.is_set():
             stats = "{}   ......   {}".format(", ".join(["{}: {}".format(f["name"] + " success ratio", "{}/{}".format(f["count_value"].value, f["failed_value"].value + f["count_value"].value)) for f in func]), ", ".join(["{}: {}".format(f["name"] + " worker", f["worker_count"].value) for f in func]) )
