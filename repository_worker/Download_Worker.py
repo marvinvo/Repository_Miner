dd42ec9c8ace87b1006468d51688f11f3e24f1cc
Marvin Vogel <91740473+marvinvo@users.noreply.github.com>
2022-08-22 13:07:47 +0200

    checkout to latest commit within specified time range

--- repository_worker/Download_Worker.py
+++ repository_worker/Download_Worker.py
@@ -1,16 +1,26 @@
 import os
 import subprocess
+from Repository_Miner.settings import FILENAME_EXTENSION_FOR_ERRORS
 from settings import FILENAME_DOWNLOAD_LOG, TIMEOUT_DOWNLOAD
 from repository_worker.WorkerError import WorkerError
 
 
 def download_worker_func(repo, repo_path, s, iolock) -> str:
     try:
-        with open(os.path.join(repo_path, FILENAME_DOWNLOAD_LOG), "w+") as f:
+        with open(os.path.join(repo_path, FILENAME_EXTENSION_FOR_ERRORS + FILENAME_DOWNLOAD_LOG), "w+") as f:
             #execute git clone
             cmd = "git clone {}".format(repo["html_url"])
             output = subprocess.check_output(cmd.split(), cwd=repo_path, timeout=TIMEOUT_DOWNLOAD, stderr=f).decode("utf-8")
-             # write log
+
+            # execute git checkout to latest commit within the commit filter time range
+            # this is actually not possible if no filter on commits have been set
+            if "commits" in repo:
+                cmd = "git checkout {}".format(repo["commits"][0]["sha"])
+                clone_path = os.path.join(repo_path, repo["name"])
+                output += subprocess.check_output(cmd.split(), cwd=clone_path, timeout=TIMEOUT_DOWNLOAD, stderr=f).decode("utf-8")
+
+        with open(os.path.join(repo_path, FILENAME_DOWNLOAD_LOG), "w+") as f:    
+            # write log
             f.write(output)
        
         return "Download Success"
