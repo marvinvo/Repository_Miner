3611ef6d34d7842233b0fa7281b11ec9cb11eaf1
Marvin Vogel <91740473+marvinvo@users.noreply.github.com>
2022-08-24 08:03:17 +0200

    a program can now be executed after each pipeline step

---
+++ demo/CamBench_fetch/cambench_fetch.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+
+RESULTS_FOLDER="/Users/marvinvogel/Downloads/test5"
+GITHUB_TOKENS="/Users/marvinvogel/Documents/Uni/CamBench/docker/CREDENTIALS.txt"
+MAX_PROCESSES="20"
+FORK="false"
+
+_bash_source=$( dirname -- "${BASH_SOURCE[0]}" )
+_execute_after_download="$_bash_source/scripts/execute_after_download.sh"
+_execute_after_compile="$_bash_source/scripts/execute_after_compile.sh"
+
+echo $_execute_after_download
+echo $_execute_after_compile
+
+python3 "$_bash_source/../../main.py" --resultsfolder "$RESULTS_FOLDER" --tokenfile "$GITHUB_TOKENS" \
+--fetch --download --execAfterDownload "zsh $_execute_after_download" --compile --execonsuccess "zsh $_execute_after_compile" \
+--maxprocesses $MAX_PROCESSES --keepclean \
+--fork $FORK 
