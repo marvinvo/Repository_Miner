7c92e431b84ca54065078030e36904432e1343a1
Marvin Vogel <91740473+marvinvo@users.noreply.github.com>
2022-10-03 15:50:32 +0200

    fix fetching

---
+++ demo/CamBench_real/docker/cambench_real.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+
+# RESULTS_FOLDER="/Users/marvinvogel/Downloads/test5"
+# GITHUB_TOKENS="/Users/marvinvogel/Documents/Uni/CamBench/docker/CREDENTIALS.txt"
+# MAX_PROCESSES="20"
+FORK="false"
+
+_bash_source=$( dirname "${BASH_SOURCE[0]}" )
+_bash_source="$(realpath "$_bash_source")"
+_execute_after_compile="$_bash_source/tools/execute_after_compile.sh"
+
+
+python3 "$_bash_source/../../main.py" --resultsfolder "$RESULTS_FOLDER" --tokenfile "$GITHUB_TOKENS" \
+--download --compile --execonsuccess "zsh $_execute_after_compile" \
+--maxprocesses $MAX_PROCESSES --writestats \
+--fork $FORK \
+--lastsort $LASTSORT
